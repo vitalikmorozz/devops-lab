61d54b57ae4006c4fb6a584d59eea6f32555357a
Vitalik Moroz <vitalik.moroz2@gmail.com>
2023-05-02 22:13:35 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = function (event, context, callback) {
+    var response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8',
+        },
+        body: "Hello world",
+    };
+    callback(null, response);
+};
