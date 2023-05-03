f0c6577ec326bcc339fba6ed4ffd7228d4bbabee
Vitalik Moroz <42621257+vitalikmorozz@users.noreply.github.com>
2023-05-03 12:07:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.handler = function (event, context, callback) {
         headers: {
             'Content-Type': 'text/html; charset=utf-8',
         },
-        body: "Hello world",
+        body: "Hello world 123",
     };
     callback(null, response);
 };
